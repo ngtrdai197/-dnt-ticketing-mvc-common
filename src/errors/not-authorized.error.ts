e54da708efa16d0bf4994a554b976b747f7a582e
Dai Nguyen <ngtrdai290197@gmail.com>
2021-09-02 16:51:00 +0700

    chore: Add auth guard, require auth middleware

--- src/errors/not-authorized.error.ts
+++ src/errors/not-authorized.error.ts
@@ -1,10 +1,10 @@
 import { CustomError, IError } from ".";
 
-export class NotAuthorizationError extends CustomError {
+export class NotAuthorizedError extends CustomError {
   public statusCode: number = 401;
   constructor(public msg: string = "Not Authorized") {
     super(msg);
-    Object.setPrototypeOf(this, NotAuthorizationError.prototype);
+    Object.setPrototypeOf(this, NotAuthorizedError.prototype);
   }
   public serializeErrors(): IError[] {
     return [{ message: this.msg }];
