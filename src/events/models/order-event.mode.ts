c4ba4fc6a406a26cb0e6afcf3fdf9abb1717cacb
Dai Nguyen <ngtrdai290197@gmail.com>
2021-10-10 01:53:12 +0700

    add order event

---
+++ src/events/models/order-event.mode.ts
@@ -0,0 +1,12 @@
+import { Subjects } from "../enums/subjects.enum";
+import { IListenerEvent } from "../base/base.listener";
+import { IOrderCreated, IOrderUpdated } from "../interfaces/order.interface";
+
+export class OrderCreatedEvent implements IListenerEvent {
+  subject!: Subjects.OrderCreated;
+  data!: IOrderCreated;
+}
+export class OrderUpdatedEvent implements IListenerEvent {
+  subject!: Subjects.OrderUpdated;
+  data!: IOrderUpdated;
+}
